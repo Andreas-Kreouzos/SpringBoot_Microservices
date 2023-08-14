6eb27a9067a2e9e0d7ad0e616974b48ee8b8065d
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 <andreas.kreouzos@hotmail.com>
2023-08-14 12:47:49 +0300

    Created api gateway

---
+++ api-gateway/src/test/java/com/andrekreou/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrekreou.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
