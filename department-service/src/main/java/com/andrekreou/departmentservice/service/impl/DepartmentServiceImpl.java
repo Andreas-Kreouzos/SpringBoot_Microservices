972c48afbc15b5e253b60985fedce629634bc8d6
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 <andreas.kreouzos@hotmail.com>
2023-08-22 18:21:13 +0300

    Added mapper for Department Service

--- department-service/src/main/java/com/andrekreou/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/andrekreou/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -2,6 +2,7 @@ package com.andrekreou.departmentservice.service.impl;
 
 import com.andrekreou.departmentservice.dto.DepartmentDto;
 import com.andrekreou.departmentservice.entity.Department;
+import com.andrekreou.departmentservice.mapper.DepartmentMapper;
 import com.andrekreou.departmentservice.repository.DepartmentRepository;
 import com.andrekreou.departmentservice.service.DepartmentService;
 import lombok.AllArgsConstructor;
@@ -15,32 +16,14 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
-        Department department = new Department(
-                departmentDto.getId(),
-                departmentDto.getDepartmentName(),
-                departmentDto.getDepartmentDescription(),
-                departmentDto.getDepartmentCode()
-        );
-
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
         Department savedDepartment = departmentRepository.save(department);
-
-        return new DepartmentDto(
-                savedDepartment.getId(),
-                savedDepartment.getDepartmentName(),
-                savedDepartment.getDepartmentDescription(),
-                savedDepartment.getDepartmentCode()
-        );
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
     }
 
     @Override
     public DepartmentDto getDepartmentByCode(String departmentCode) {
         Department department = departmentRepository.findByDepartmentCode(departmentCode);
-
-        return new DepartmentDto(
-                department.getId(),
-                department.getDepartmentName(),
-                department.getDepartmentDescription(),
-                department.getDepartmentCode()
-        );
+        return DepartmentMapper.mapToDepartmentDto(department);
     }
 }
