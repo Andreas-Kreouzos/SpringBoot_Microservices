da907ae479bd66f12ecbf7d3395dbb0dc8f8fb3e
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 <andreas.kreouzos@hotmail.com>
2023-08-27 16:57:01 +0300

    API Documentation for Organisation Service

--- organisation-service/src/main/java/com/andrekreou/organisationservice/OrganisationServiceApplication.java
+++ organisation-service/src/main/java/com/andrekreou/organisationservice/OrganisationServiceApplication.java
@@ -1,8 +1,33 @@
 package com.andrekreou.organisationservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Organisation Service REST APIs",
+				description = "Organisation Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Andreas",
+						email = "andreas.kreouzos@hotmail.com",
+						url = "https://www.linkedin.com/in/andreas-kreouzos"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/andreas-kreouzos"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Organisation Service Doc",
+				url = "https://www.javaguides.net/"
+		)
+)
 @SpringBootApplication
 public class OrganisationServiceApplication {
 
--- organisation-service/src/main/java/com/andrekreou/organisationservice/controller/OrganisationController.java
+++ organisation-service/src/main/java/com/andrekreou/organisationservice/controller/OrganisationController.java
@@ -2,6 +2,9 @@ package com.andrekreou.organisationservice.controller;
 
 import com.andrekreou.organisationservice.dto.OrganisationDto;
 import com.andrekreou.organisationservice.service.OrganisationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Organisation Service - Organisation Controller",
+        description = "Organisation Controller Exposes REST APIs for Organisation Service"
+)
 @RestController
 @RequestMapping("api/organisations")
 @AllArgsConstructor
@@ -19,12 +26,28 @@ public class OrganisationController {
 
     private OrganisationService organisationService;
 
+    @Operation(
+            summary = "Save Organisation REST API",
+            description = "Save Organisation REST API is used to save organisation object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<OrganisationDto> saveOrganisation(@RequestBody OrganisationDto organisationDto) {
         OrganisationDto savedOrganisation = organisationService.saveOrganisation(organisationDto);
         return new ResponseEntity<>(savedOrganisation, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Organisation REST API",
+            description = "Get Organisation REST API is used to get a single organisation object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{code}")
     public ResponseEntity<OrganisationDto> getOrganisation(@PathVariable("code") String organisationCode) {
         OrganisationDto organisationDto = organisationService.getOrganisationByCode(organisationCode);
