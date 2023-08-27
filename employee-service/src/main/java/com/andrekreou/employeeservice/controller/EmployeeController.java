88cb83a3ffc86a0c1a4685ebe643433c4ab6df54
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 <andreas.kreouzos@hotmail.com>
2023-08-27 16:56:42 +0300

    API Documentation for Employee Service

--- employee-service/src/main/java/com/andrekreou/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/andrekreou/employeeservice/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.andrekreou.employeeservice.controller;
 import com.andrekreou.employeeservice.dto.APIResponseDto;
 import com.andrekreou.employeeservice.dto.EmployeeDto;
 import com.andrekreou.employeeservice.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Employee Service - Employee Controller",
+        description = "Employee Controller Exposes REST APIs for Employee Service"
+)
 @RestController
 @RequestMapping("api/employees")
 @AllArgsConstructor
@@ -20,12 +27,28 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "Save Employee REST API",
+            description = "Save Employee REST API is used to save employee object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Employee REST API",
+            description = "Get Employee REST API is used to get an employee object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId) {
         APIResponseDto apiResponseDto = employeeService.getEmployeeById(employeeId);
